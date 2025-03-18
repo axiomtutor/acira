6318b134988341a1d34e773ef5468c911c61e7cd
axiomtutor <axiomtutor@protonmail.com>
2025-03-18 18:54:17 -0400

    orders

--- ACIRAM.py
+++ ACIRAM.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.11.20"
+__generated_with = "0.11.22"
 app = marimo.App(width="medium")
 
 
@@ -1100,43 +1100,103 @@ def _(mo):
     return
 
 
-@app.cell
-def _():
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        The field properties contain what we will need to know about the algebraic operations of $+$ and $\times$ for the real numbers.
+
+        We now proceed likewise, to describe how the $\le$ relation on the real numbers will behave.  
+        """
+    )
     return
 
 
-@app.cell
-def _():
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        Let's first consider the ordering relation $\le$ on the natural numbers.
+
+        We call $(\Bbb N,\le)$ a "partially ordered set" because the $\le$ relation satisfies some basic properties that we expect from an ordering of a set.
+        """
+    )
     return
 
 
-@app.cell
-def _():
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        For one thing it is reflexive:  For any $a\in \Bbb N$ we have $a\le a$.  
+
+        For another it is anti-symmetric:  If $a,b\in \Bbb N$ and we have both $a\le b$ and $b\le a$ then it follows that $a=b$.
+
+        And finally, it is transitive: $a\le b$ and $b\le c$ imply $a\le c$.
+        """
+    )
     return
 
 
-@app.cell
-def _():
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""Of course, all the same could just as well be said for the rational numbers, so this too is partially ordered by its standard ordering $\le$.  """)
     return
 
 
-@app.cell
-def _():
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        A more provocative example is the subset relation.
+
+        Let $X$ be a nonempty set, and set $Y =\mathcal P(X)$, the powerset of $X$.  
+
+        Define the following relation, $\preceq$, on the set $Y$.
+
+        $$ A\preceq B \Leftrightarrow A\subseteq B, \quad \forall A,B\in Y $$
+        """
+    )
     return
 
 
-@app.cell
-def _():
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        One can readily check that this is also reflexive, anti-symmetric, and transitive.
+
+        *Reflexive*: For any $A\in Y$ we have $A\subseteq A$ and therefore $A\preceq A$.  
+        """
+    )
     return
 
 
-@app.cell
-def _():
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""*Anti-symmetric*: Let $A,B\in Y$.  If $A\subseteq B$ and $B\subseteq A$ it follows by basic set theory that $A=B$.  """)
     return
 
 
-@app.cell
-def _():
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""*Transitive*:  Let $A,B,C\in Y$.  Assume $A\subseteq B$ and $B\subseteq C$.  Then, again by basic set theory, $A\subseteq C$.  """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.callout(mo.md(r"""
+    Let $X$ be a nonempty set and $\preceq\subseteq X^2$ a relation.
+
+    If the following conditions are met, we call $\preceq$ a **partial order**.
+
+    1. $\preceq$ is reflexive.
+
+    2. $\preceq$ is anti-symmetric.
+
+    3. $\preceq$ is transitive.
+    """), kind="success")
     return
 
 
