29ccf1b4182f52701025c74532c123c223948cf2
axiomtutor <axiomtutor@protonmail.com>
2025-03-24 14:57:29 -0400

    ref groups

--- ACIRA_unit1_ref.py
+++ ACIRA_unit1_ref.py
@@ -30,7 +30,7 @@ def _(mo):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""## 0001 Groups""")
+    mo.md(r"""# 0001 Groups""")
     return
 
 
@@ -79,9 +79,16 @@ def _(mo):
 
 
 @app.cell(hide_code=True)
-def _(mo):
-    mo.callout(mo.md(r"""
-    Group theorem 1
+def _():
+    thmlst = []
+    return (thmlst,)
+
+
+@app.cell(hide_code=True)
+def _(mo, thmlst):
+    thmlst.append(1)
+    mo.output.replace(mo.callout(mo.md(r"""
+    Group theorem """ + f"{len(thmlst)}" + r"""
 
     `Unique associative operation identity, and inverses`
 
@@ -90,7 +97,7 @@ def _(mo):
     Let $X$ be a nonempty set, $\ast:X^2\to X$ an operation on $X$, and $e\in X$ an identity element.  Also let $a\in X$ and suppose $a^{-1}$ exists, i.e. $aa^{-1}=e$.
 
     Then $e$ is the unique identity for $\ast$, and also $a^{-1}$ is the unique inverse of $a$.
-    """), kind="danger")
+    """), kind="danger"))
     return
 
 
@@ -98,15 +105,27 @@ def _(mo):
 def _(mo):
     mo.md(
         r"""
-        *Proof*: Let $y\in G$ have the identity property.  Then 
+        *Proof*: Let $y\in X$ have the identity property.  Then 
 
         $$ ye = y = e $$
 
         where the first equation is by the identity property of $e$, and the second is by the identity property of $y$.
 
-        Let $x\in G$ and let $y\in G$ have the inverse property for $x$, i.e. $yx=e$.
+        Therefore $e$ is the unique identity for $\ast$.
 
-        $$ 
+        ---
+
+        Let $a\in X$ with $a^{-1}\in X$ an inverse of $a$.  Let $y\in X$ have the inverse property for $a$, i.e. $ya=e$.
+
+        \begin{align*}
+          a^{-1} &= a^{-1}e \\
+          &= a^{-1}(ay) \\
+          &= (a^{-1}a)y \\
+          &= ey \\
+          &= y
+        \end{align*}
+
+        Therefore $a^{-1}$ is the unique inverse of $a$. 
 
         $\Box$
         """
@@ -117,7 +136,65 @@ def _(mo):
 @app.cell(hide_code=True)
 def _(mo):
     mo.callout(mo.md(r"""
-    Group theorem 2
+    Group theorem
+
+    `Unique group identity and inverse`
+
+    ---
+
+    The identity in $G$ is unique, and each $x\in G$ has a unique inverse.
+
+    """), kind="danger")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        *Proof*: Since the group operation is associative, by `Unique associative operation identity and inverses`, the identity and inverses are unique in $G$.
+
+        $\Box$
+        """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.callout(mo.md(f"""
+    Group theorem 
+
+    `Group inverse inverse`
+
+    ---""" + r"""
+
+    For all $x\in G$ we have $(x^{-1})^{-1}=x$.
+    """), kind="danger")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        *Proof*: Because $x^{-1}x=e$, this show that $x$ has the property of being the inverse of $x^{-1}$.  
+
+        Since the inverse of $x^{-1}$ is uniquely $(x^{-1})^{-1}$, by the `Unique group identity and inverse` theorem, then 
+
+        $$ x = (x^{-1})^{-1} $$
+
+
+        $\Box$
+        """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.callout(mo.md(r"""
+    Group theorem 3
 
     `Inverse of product`
 
@@ -132,6 +209,30 @@ def _(mo):
 
 @app.cell(hide_code=True)
 def _(mo):
+    mo.md(
+        r"""
+        *Proof*: 
+
+        \begin{align*}
+        (ab)(b^{-1}a^{-1}) &= a(bb^{-1})a^{-1} \\
+        &= a e a^{-1}\\
+        &= e
+        \end{align*}
+
+        So $b^{-1}a^{-1}$ has the property of the inverse of $ab$.
+
+        Then by the `Unique group identity and inverse` theorem, 
+
+        $$b^{-1}a^{-1} = (ab)^{-1} $$
+
+        $\Box$
+        """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
     mo.callout(mo.md(r"""
     Group theorem 3
 
@@ -139,7 +240,7 @@ def _(mo):
 
     ---
 
-    For ever $m,n\in\Bbb Z$ and for $a\in G$, we have 
+    For every $m,n\in\Bbb Z$ and for $a\in G$, we have 
 
     $$ a^ma^n = a^{m+n},\quad (a^m)^n $$
 
@@ -147,6 +248,98 @@ def _(mo):
 
     $$ a^mb^m = (ab)^m $$
     """), kind="danger")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    _case1 = mo.callout(mo.md(r"""
+    *Case 1*: Assume $m=0$ or $n=0$.  
+
+    Without loss of generality, specifically assume $m=0$.
+
+    Then $a^ma^n = ea^n = a^n$ which is the same as $a^{m+n}=a^n$.
+    """))
+
+    _case2 = mo.callout(mo.md(r"""
+    *Case 2*: Assume $0<m,n$.
+
+    Simply by counting terms,
+
+    \begin{align*}
+    a^ma^n &= \overbrace{aa\cdots a}^m \ast \overbrace{aa\cdots a}^n \\
+    &= \overbrace{aa\cdots a}^{m+n}\\
+    &= a^{m+n}
+    \end{align*}
+    """))
+
+    _case3 = mo.callout(mo.md(r"""
+    *Case 3*: Assume $m<0<n$ and $-m \le n$.
+
+    \begin{align*}
+      a^ma^n &= \overbrace{a^{-1}a^{-1}\cdots a^{-1}}^{-m}\ast \overbrace{aa\cdots a}^n \\
+      &= \overbrace{a^{-1}a^{-1}\cdots a^{-1}}^{-m-1}\ast e \ast \overbrace{aa\cdots a}^{n-1} \\
+      &\vdots \\
+      &= \overbrace{aa\cdots a}^{n-(-m)} \\
+      &=a^{n+m}
+    \end{align*}
+    """))
+
+    _case4 = mo.callout(mo.md(r"""
+    *Case 4*: Assume $m,n<0$.  
+
+    \begin{align*}
+      a^ma^n &= \overbrace{a^{-1}a^{-1}\cdots a^{-1}}^{-m}\ast \overbrace{a^{-1}a^{-1}\cdots a^{-1}}^{-n}\\
+      &= \overbrace{a^{-1}a^{-1}\cdots a^{-1}}^{-(m+n)}\\
+      &= a^{m+n}
+    \end{align*}
+    """))
+
+    mo.md(f" *Proof*: " + r"Assume $m\le n$." +f"""
+    {_case1}
+
+    {_case2}
+
+    {_case3}
+
+    {_case4}
+
+    """+r"""*Mutatis mutandis* the proof is the same if $n\le m$.
+
+    $\Box$
+    """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""# 0003 Rings and Fields""")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.callout(mo.md(r"""
+    Let $R$ be a nonempty set with two operations $+,\times:R^2\to R$.  
+
+    We say that $\times$ **distributes over $+$** if $\forall a,b,c\in R$,
+
+    $$ a\times (b+c) = (a\times b) + (a\times c) $$
+
+    We say that $(R,+,\times)$ is a **ring** if 
+
+    1. Both operations are associative.
+    2. $(R,+)$ is a commutative group.  We write its identity as $0$.
+    3. $\times$ has an identity element.   We write it as $1$.
+    4. $\times$ distributes over $+$.
+
+    For brevity, we write the inverse of $x\in R$ as $-x$ and we write $a+(-b)$ instead as $a-b$ for all $a,b\in R$.  We also write $a\times b$ as $ab$ and $a\times b^{-1}$ as $a/b$.  
+
+    For $n\in\Bbb Z$ and $a\in R$, we define $na$ as repeated addition, in the exact same sense as we did for a group.  Likewise $a^n$ is repeated multiplication, in the sense we defined for a group.  
+
+    If $(R,\times)$ forms a commutative group, we say that $(R,+,\times)$ is a **field**.
+    """), kind="success")
     return
 
 
