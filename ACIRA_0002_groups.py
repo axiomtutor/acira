1a15279343695361274246a72c312406172adf68
axiomtutor <axiomtutor@protonmail.com>
2025-05-30 14:23:16 -0400

    edit

--- ACIRA_0002_groups.py
+++ ACIRA_0002_groups.py
@@ -1,14 +1,12 @@
 import marimo
 
-__generated_with = "0.13.10"
+__generated_with = "0.13.14"
 app = marimo.App(width="medium")
 
 
 @app.cell(hide_code=True)
 def _():
     import marimo as mo
-    import matplotlib.pyplot as plt
-    import numpy as np
     return (mo,)
 
 
